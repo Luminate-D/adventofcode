1a2815463089ef1afacf58d97f12b6f128c03be2
Roman Yerchyk <lann.wauthority@gmail.com>
2025-12-02 17:43:04 +0200

    benchmark just why not

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 use crate::day_1::day_1;
 use crate::day_2::day_2;
 
@@ -6,7 +7,12 @@ mod day_2;
 
 fn main() {
     println!("Hello, baka!");
-    
+
+    let now = Instant::now();
     // day_1();
     day_2();
+
+    let elapsed = now.elapsed();
+
+    println!("Time elapsed: {:?}", elapsed);
 }
